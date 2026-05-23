e9a85039e4017cb25109b321e9164305ced6dd3f
breakintoprogram <dean.belfield@gmail.com>
2026-05-23 10:49:15 +0100

    Fixed ALU flag bugs

--- registers.cpp
+++ registers.cpp
@@ -17,8 +17,8 @@ void registers::ex(REG* rp1, REG* rp2)
 void registers::A_add(uint8_t d) {
 	uint16_t w = AF.A + d;
 	uint8_t  b = w & 0xFF;
-	AF.C = (w >= 0xFF);
-	AF.S = (b >= 0x7F);
+	AF.C = (w > 0xFF);
+	AF.S = (b > 0x7F);
 	AF.Z = (b == 0x00);
 	AF.A = b;
 
@@ -27,8 +27,8 @@ void registers::A_add(uint8_t d) {
 void registers::A_adc(uint8_t d) {
 	uint16_t w = AF.A + d + AF.C;
 	uint8_t  b = w & 0xFF;
-	AF.C = (w >= 0xFF);
-	AF.S = (b >= 0x7F);
+	AF.C = (w > 0xFF);
+	AF.S = (b > 0x7F);
 	AF.Z = (b == 0x00);
 	AF.A = b;
 }
@@ -36,8 +36,8 @@ void registers::A_adc(uint8_t d) {
 void registers::A_sub(uint8_t d) {
 	uint16_t w = AF.A - d;
 	uint8_t  b = w & 0xFF;
-	AF.C = (w >= 0xFF);
-	AF.S = (b >= 0x7F);
+	AF.C = (w > 0xFF);
+	AF.S = (b > 0x7F);
 	AF.Z = (b == 0x00);
 	AF.A = b;
 }
@@ -45,8 +45,8 @@ void registers::A_sub(uint8_t d) {
 void registers::A_sbc(uint8_t d) {
 	uint16_t w = AF.A - d - AF.C;
 	uint8_t  b = w & 0xFF;
-	AF.C = (w >= 0xFF);
-	AF.S = (b >= 0x7F);
+	AF.C = (w > 0xFF);
+	AF.S = (b > 0x7F);
 	AF.Z = (b == 0x00);
 	AF.A = b;
 }
@@ -54,35 +54,35 @@ void registers::A_sbc(uint8_t d) {
 void registers::A_and(uint8_t d) {
 	AF.L = 0;
 	AF.A &= d;
+	AF.S = (AF.A > 0x7F);
 	AF.Z = (AF.A == 0x00);
-	AF.S = (AF.A >= 0x7F);
 }
 
 void registers::A_xor(uint8_t d) {
 	AF.L = 0;
 	AF.A ^= d;
+	AF.S = (AF.A > 0x7F);
 	AF.Z = (AF.A == 0x00);
-	AF.S = (AF.A >= 0x7F);
 }
 void registers::A_or(uint8_t d) {
 	AF.L = 0;
 	AF.A |= d;
+	AF.S = (AF.A > 0x7F);
 	AF.Z = (AF.A == 0x00);
-	AF.S = (AF.A >= 0x7F);
 }
 
 void registers::A_cp(uint8_t d) {
 	uint16_t w = AF.A - d;
 	uint8_t  b = w & 0xFF;
-	AF.C = (w >= 0xFF);
-	AF.S = (b >= 0x7F);
+	AF.C = (w > 0xFF);
+	AF.S = (b > 0x7F);
 	AF.Z = (b == 0x00);
 }
 
 void registers::A_neg() {
 	AF.C = (AF.A != 0x00);
 	AF.A=-AF.A;
-	AF.S = (AF.A >= 0x7F);
+	AF.S = (AF.A > 0x7F);
 	AF.Z = (AF.A == 0x00);
 }
 
