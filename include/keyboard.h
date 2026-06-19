b47f26397cf70598314cde31357c6276ba93c666
Dean Belfield <36993476+breakintoprogram@users.noreply.github.com>
2026-06-19 21:35:04 +0100

    Now uses a keyboard lookup table

--- include/keyboard.h
+++ include/keyboard.h
@@ -10,6 +10,7 @@
 #pragma once
 
 #include <cstdint>
+#include <map>
 #include <SDL2/SDL.h>
 
 #include "ports.h"
@@ -17,13 +18,64 @@
 
 using namespace std;
 
+// Represents a single Spectrum key
+//
+struct Key {
+	uint8_t port;	// The Z80 port the key is on
+	uint8_t column;	// The column (bit) in that port
+};
+
 class Keyboard {
 public:
     Keyboard(Ports* ports);
-    ~Keyboard();
 
     void   press(SDL_Keycode sym, bool pressed);
 private:
     Ports* ports;
     void   port(uint8_t p, uint8_t bit, bool pressed);
+
+	// Map of SDL2 keypresses to Spectrum port and columns
+	//
+	map<SDL_Keycode, Key> keys = {
+        { SDLK_LSHIFT,	{ 0xFE, 0 } },
+        { SDLK_z,		{ 0xFE, 1 } },
+        { SDLK_x,		{ 0xFE, 2 } },
+        { SDLK_c,		{ 0xFE, 3 } },
+        { SDLK_v,		{ 0xFE, 4 } },
+        { SDLK_a,		{ 0xFD, 0 } },
+        { SDLK_s,		{ 0xFD, 1 } },
+        { SDLK_d,		{ 0xFD, 2 } },
+        { SDLK_f,		{ 0xFD, 3 } },
+        { SDLK_g,		{ 0xFD, 4 } },
+        { SDLK_q,		{ 0xFB, 0 } },
+        { SDLK_w,		{ 0xFB, 1 } },
+        { SDLK_e,		{ 0xFB, 2 } },
+        { SDLK_r,		{ 0xFB, 3 } },
+        { SDLK_t,		{ 0xFB, 4 } },
+        { SDLK_1,		{ 0xF7, 0 } },
+        { SDLK_2,		{ 0xF7, 1 } },
+        { SDLK_3,		{ 0xF7, 2 } },
+        { SDLK_4,		{ 0xF7, 3 } },
+        { SDLK_5,		{ 0xF7, 4 } },
+        { SDLK_0,		{ 0xEF, 0 } },
+        { SDLK_9,		{ 0xEF, 1 } },
+        { SDLK_8,		{ 0xEF, 2 } },
+        { SDLK_7,		{ 0xEF, 3 } },
+        { SDLK_6,		{ 0xEF, 4 } },
+        { SDLK_p,		{ 0xDF, 0 } },
+        { SDLK_o,		{ 0xDF, 1 } },
+        { SDLK_i,		{ 0xDF, 2 } },
+        { SDLK_u,		{ 0xDF, 3 } },
+        { SDLK_y,		{ 0xDF, 4 } },
+        { SDLK_RETURN,	{ 0xBF, 0 } },
+        { SDLK_l,		{ 0xBF, 1 } },
+        { SDLK_k,		{ 0xBF, 2 } },
+        { SDLK_j,		{ 0xBF, 3 } },
+        { SDLK_h,		{ 0xBF, 4 } },
+        { SDLK_SPACE,	{ 0x7F, 0 } },
+        { SDLK_RSHIFT,	{ 0x7F, 1 } },
+        { SDLK_m,		{ 0x7F, 2 } },
+        { SDLK_n,		{ 0x7F, 3 } },
+        { SDLK_b,		{ 0x7F, 4 } },
+	};
 };
--- keyboard.cpp
+++ keyboard.cpp
@@ -13,59 +13,9 @@ Keyboard::Keyboard(Ports* ports) : ports(ports)
 {
 }
 
-Keyboard::~Keyboard() {
-}
-
 void Keyboard::press(SDL_Keycode sym, bool pressed) {
-    switch(sym) {
-        case SDLK_LSHIFT: port(0xFE, 0, pressed); break;
-        case SDLK_z: port(0xFE, 1, pressed); break;
-        case SDLK_x: port(0xFE, 2, pressed); break;
-        case SDLK_c: port(0xFE, 3, pressed); break;
-        case SDLK_v: port(0xFE, 4, pressed); break;
-
-        case SDLK_a: port(0xFD, 0, pressed); break;
-        case SDLK_s: port(0xFD, 1, pressed); break;
-        case SDLK_d: port(0xFD, 2, pressed); break;
-        case SDLK_f: port(0xFD, 3, pressed); break;
-        case SDLK_g: port(0xFD, 4, pressed); break;
-
-        case SDLK_q: port(0xFB, 0, pressed); break;
-        case SDLK_w: port(0xFB, 1, pressed); break;
-        case SDLK_e: port(0xFB, 2, pressed); break;
-        case SDLK_r: port(0xFB, 3, pressed); break;
-        case SDLK_t: port(0xFB, 4, pressed); break;
-
-        case SDLK_1: port(0xF7, 0, pressed); break;
-        case SDLK_2: port(0xF7, 1, pressed); break;
-        case SDLK_3: port(0xF7, 2, pressed); break;
-        case SDLK_4: port(0xF7, 3, pressed); break;
-        case SDLK_5: port(0xF7, 4, pressed); break;
-
-        case SDLK_0: port(0xEF, 0, pressed); break;
-        case SDLK_9: port(0xEF, 1, pressed); break;
-        case SDLK_8: port(0xEF, 2, pressed); break;
-        case SDLK_7: port(0xEF, 3, pressed); break;
-        case SDLK_6: port(0xEF, 4, pressed); break;
-
-        case SDLK_p: port(0xDF, 0, pressed); break;
-        case SDLK_o: port(0xDF, 1, pressed); break;
-        case SDLK_i: port(0xDF, 2, pressed); break;
-        case SDLK_u: port(0xDF, 3, pressed); break;
-        case SDLK_y: port(0xDF, 4, pressed); break;
-
-        case SDLK_RETURN: port(0xBF, 0, pressed); break;
-        case SDLK_l: port(0xBF, 1, pressed); break;
-        case SDLK_k: port(0xBF, 2, pressed); break;
-        case SDLK_j: port(0xBF, 3, pressed); break;
-        case SDLK_h: port(0xBF, 4, pressed); break;
-
-        case SDLK_SPACE: port(0x7F, 0, pressed); break;
-        case SDLK_RSHIFT: port(0x7F, 1, pressed); break;
-        case SDLK_m: port(0x7F, 2, pressed); break;
-        case SDLK_n: port(0x7F, 3, pressed); break;
-        case SDLK_b: port(0x7F, 4, pressed); break;
-    }
+	Key k = keys[sym];
+	port(k.port, k.column, pressed);
 }
 
 void Keyboard::port(uint8_t p, uint8_t bit, bool pressed) {
