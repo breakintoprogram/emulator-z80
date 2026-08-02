06465b3d171741b5865e6b6d098d482bc6f2e5cd
breakintoprogram <36993476+breakintoprogram@users.noreply.github.com>
2026-08-02 10:23:10 +0100

    Refactor for Emscripten (#2)
    * Refactored main emulator loop
    * Fixed single-stepping bug
    * Fixed bug in interrupt service logic
    * Moved loop logic into its own function
    * Added default scale

---
+++ emulator.cpp
@@ -0,0 +1,126 @@
+//
+// Title:	        Main emulator class
+// Description:		
+// Author:	        Dean Belfield
+// Created:	        28/07/2026
+// Last Updated:	28/07/2026
+//
+// Modinfo:
+
+#include "emulator.h"
+
+Emulator::Emulator(int scale, string filename) : step(false), interrupts(true) 
+{
+	logger = new Logger(cout);				// Logging class
+
+	mem = new Mem(							// Memory interface with lambda for 'is RAM' check
+		[](uint16_t address)->bool {
+			return address >= 0x4000;
+		}
+	);
+	ports = new Ports();					// I/O ports (keyboard, ULA)
+	tape = new Tape(ports);					// Tape interface	
+
+	// Load the ROM in
+	//
+	if (!mem->load(0x0000, code)) {
+		throw runtime_error("Could not open ROM file");
+	}; 	
+
+	// Initialise the rest of the system
+	//
+	ula = new Ula(mem, ports, scale);	// ULA (video)
+	keyboard = new Keyboard(ports);		// Keyboard interface
+	z80 = new Z80(mem, ports, logger);	// The Z80 CPU itself
+
+	// Setup the tape if a filename as been passed
+	//
+	if(filename.length() > 0) {
+		if(!tape->open(filename)) {
+			throw runtime_error("Could not open tape file");
+		}
+	}
+	z80->reset();							// Reset the Z80	
+}
+
+Emulator::~Emulator()
+{
+	delete logger;
+	delete mem;
+	delete ports;
+	delete keyboard;
+	delete ula;
+	delete tape;
+	delete z80;	
+}
+
+void Emulator::handleEvents(SDL_Event &e) {
+	switch (e.type) {
+		//
+		// Keypress down events
+		//
+		case SDL_KEYDOWN: {
+			switch (e.key.keysym.sym) {
+				case SDLK_F10: tape->start(); break;
+				case SDLK_F11: tape->stop(); break;
+				case SDLK_F12: z80->setSingleStep(true); break;
+			}
+			if(z80->getSingleStep()) {
+				switch (e.key.keysym.sym) {
+					case SDLK_RETURN: step = true; break;
+					case SDLK_d: interrupts = false; break;
+					case SDLK_e: interrupts = true; break;
+					case SDLK_t: z80->setTrace(!z80->getTrace()); break;
+					case SDLK_g: z80->setSingleStep(false); break;
+					case SDLK_o: z80->dump(); break;
+					case SDLK_r: z80->reset(); break;
+				}
+			}
+			else {
+				keyboard->press(e.key.keysym.sym, true);
+			}
+		} break;
+		//
+		// Keypress up events
+		//
+		case SDL_KEYUP: {
+			keyboard->press(e.key.keysym.sym, false);
+		} break;
+	}
+}
+
+void Emulator::run() {
+	uint16_t t;
+	//
+	// Process CPU cycle(s)
+	//
+	if (z80->getSingleStep()) {					// If in single-step mode
+		if (step) {								// If a step has been requested by the user
+			step = false;						// Flag the step as being done
+			z80->run();							// Run the CPU
+			t = z80->getT();
+			tape->play(t);						// Play any inserted cassette
+			ula->render(t);						// Render a number of pixels
+		}
+	}
+	else {										// Otherwise if we're in free-running mode
+		do {
+			z80->run();							// Run the CPU
+			t = z80->getT();					// Get the T-states
+			tape->play(t);						// Play any inserted cassette
+			ula->render(t);						// Render a number of pixels
+		} while (!ula->getvBlank());			// Loop until we've got a vblank
+	}
+	//
+	// Service interrupts
+	//
+	if (ula->getvBlank()) {						// If we've hit a vblank then
+		ula->setvBlank(false);					// Clear the vblank
+		//
+		// Only service the interrupt if we're not single-stepping or interrupts are enabled in single-step mode
+		//
+		if (!z80->getSingleStep() || interrupts) {
+			z80->interruptRequest();
+		}		
+	}
+}
--- main.cpp
+++ main.cpp
@@ -15,57 +15,59 @@
 #include <chrono>
 #include <thread>
 
-#include "defines.h"
-#include "logger.h"
-#include "ula.h"
-#include "keyboard.h"
-#include "memory.h"
-#include "ports.h"
-#include "tape.h"
-#include "z80.h"
-
-#define code "roms/48.rom"
+#include "emulator.h"
 
 using namespace std::chrono;
 using namespace std::literals::chrono_literals;
 
-Logger*   logger;
-Keyboard* keyboard;
-Ula*      ula;
-Z80*      z80;
-Mem*      mem;
-Ports*    ports;
-Tape*     tape;
+// Global variables
+//
+static bool quit = false;
+static auto speed = 20000us;
 
-void cleanup() {
-	delete logger;
-	delete mem;
-	delete ports;
-	delete keyboard;
-	delete ula;
-	delete tape;
-	delete z80;
+// The loop
+// Parameters:
+// - arg: void pointer to an Emulator object
+//
+void loop(void* arg) {
+	SDL_Event e;
+
+	Emulator* emulator = (Emulator*)arg;
+
+	while (SDL_PollEvent(&e) != 0) {
+		switch (e.type) {
+			case SDL_QUIT: {
+				quit = true;
+			} break;	
+			case SDL_KEYDOWN: {
+				switch (e.key.keysym.sym) {
+					case SDLK_F1: speed = 20000us; break;
+					case SDLK_F2: speed = 10000us; break;
+					case SDLK_F3: speed =  5000us; break;
+					case SDLK_F4: speed =  2500us; break;					
+				}
+			} break;
+		}
+		emulator->handleEvents(e);
+	}
+	try {
+		emulator->run();
+	}
+	catch(const runtime_error& e) {
+		cout << "Error: " << e.what() << endl;
+	}
 }
 
+// The main function
+// Parameters:
+// - argc: argument count
+// - argv: argument array
+//
 int main(int argc, char* argv[])
 {
-	bool       quit = false;
-	bool       step = false;
-	bool       interrupts = true;
-	auto       speed = 20000us;
-	int        scale = 1;
-	SDL_Event  e;
-	auto       nextTick = steady_clock::now() + speed;
-
-	logger = new Logger(cout);			// Logging class
-
-	mem = new Mem(						// Memory interface with lambda for 'is RAM' check
-		[](uint16_t address)->bool {
-			return address >= 0x4000;
-		}
-	);
-	ports = new Ports();				// I/O ports (keyboard, ULA)
-	tape = new Tape(ports);				// Tape interface
+	Emulator* emulator;
+	string    filename;
+	int       scale = 1; 
 
 	// Handle any command-line parameters
 	//
@@ -82,120 +84,27 @@ int main(int argc, char* argv[])
 		}
 
 		if (token == "l" || token == "load") {
-			cout << "Loading tape file " << parameter << endl;
-			if (!tape->open(parameter)) {
-				cout << "Error: Could not open tape file" << endl;
-				cleanup();
-				return 1;
-			}
+			filename = parameter;
 			continue;
 		}
 	}
 
-	// Load the ROM in
-	//
-	if (!mem->load(0x0000, code)) {
-		cout << "Error loading '" << code << "'." << endl;
-		cleanup();
-		return 1;
-	}; 
-	
-	// Initialise the rest of the system
-	//
-	try  {
-		ula = new Ula(mem, ports, scale);	// ULA (video)
-		keyboard = new Keyboard(ports);		// Keyboard interface
-		z80 = new Z80(mem, ports, logger);	// The Z80 CPU itself
+	try {
+		emulator = new Emulator(scale, filename);
 	}
-	catch(const exception& e) {
+	catch(const runtime_error& e) {
 		cout << "Error: " << e.what() << endl;
-		cleanup();
-		return 1;
+		delete emulator;
+		return 0;
 	}
-
-	z80->reset();						// Reset the Z80
 
 	// The main loop
 	//
 	while (!quit) {
-		while (SDL_PollEvent(&e) != 0) {
-			switch (e.type) {
-				//
-				// User closes window
-				//
-				case SDL_QUIT: {
-					quit = true;
-				} break;
-				//
-				// Keyboard events
-				//
-				case SDL_KEYDOWN: {
-					switch (e.key.keysym.sym) {
-						case SDLK_F1: speed = 20000us; break;
-						case SDLK_F2: speed = 10000us; break;
-						case SDLK_F3: speed =  5000us; break;
-						case SDLK_F4: speed =  2500us; break;
-						case SDLK_F10: tape->start(); break;
-						case SDLK_F11: tape->stop(); break;
-						case SDLK_F12: z80->setSingleStep(true); break;
-					}
-					if(z80->getSingleStep()) {
-						switch (e.key.keysym.sym) {
-							case SDLK_RETURN: step = true; break;
-							case SDLK_d: interrupts = false; break;
-							case SDLK_e: interrupts = true; break;
-							case SDLK_t: z80->setTrace(!z80->getTrace()); break;
-							case SDLK_g: z80->setSingleStep(false); break;
-							case SDLK_o: z80->dump(); break;
-							case SDLK_r: z80->reset(); break;
-						}
-					}
-					else {
-						keyboard->press(e.key.keysym.sym, true);
-					}
-				} break;
-				case SDL_KEYUP: {
-					keyboard->press(e.key.keysym.sym, false);
-				} break;
-			}
-		}
-		//
-		// Process CPU cycle(s)
-		//
-		uint32_t tcnt = 0;
-		uint16_t t;
-		try {
-			if (z80->getSingleStep()) {						// Single-step mode
-				if (step) {
-					step = false;							// Flag the step as being done
-					z80->run();								// Run the CPU
-					t = z80->getT();
-					tape->play(t);							// Play any inserted cassette
-					ula->render(t);							// Render a number of pixels
-				}
-			}
-			else {											// Normal running mode
-				while (tcnt < 448 && !ula->getvBlank()) {	// Loop for around 2 scanlines (224 x 2)
-					z80->run();								// Run the CPU
-					t = z80->getT();						// Get the T-states
-					tcnt += t;								// Add to the running total
-					tape->play(t);							// Play any inserted cassette
-					ula->render(t);							// Render a number of pixels
-				}
-			}
-			if (ula->getvBlank()) {							// On the vblank
-				ula->setvBlank(false);						// Service any interrupts
-				if (!z80->getSingleStep() || interrupts) {	// Provided we're not single-stepping and they're enabled
-					z80->interruptRequest();
-				}
-				this_thread::sleep_until(nextTick);			// Wait until the next 1/50th of a second
-				nextTick = steady_clock::now() + speed;		// Set the next tick to be from now
-			}
-		}
-		catch(const exception& e) {
-			cout << "Error: " << e.what() << endl;
-		}
+		auto nextTick = steady_clock::now() + speed;	
+		loop((void *)emulator);	
+		this_thread::sleep_until(nextTick);			// Wait until the next 1/50th of a second
+		nextTick = steady_clock::now() + speed;		// Set the next tick to be from now		
 	}
-	cleanup();	
 	return 0;
 }
