63bdfd6d885103925c7a701408bfc11163647c9d
breakintoprogram <36993476+breakintoprogram@users.noreply.github.com>
2026-08-02 10:23:10 +0100

    1 refactor for emscripten (#2)
    * Refactored main emulator loop
    * Fixed single-stepping bug
    * Fixed bug in interrupt service logic
    * Moved loop logic into its own function
    * Added default scale

---
+++ include/emulator.h
@@ -0,0 +1,47 @@
+//
+// Title:	        Main emulator class
+// Description:		
+// Author:	        Dean Belfield
+// Created:	        28/07/2026
+// Last Updated:	28/07/2026
+//
+// Modinfo:
+
+#pragma once
+
+#include <cstdint>
+#include <SDL2/SDL.h>
+
+#include "defines.h"
+#include "logger.h"
+#include "ula.h"
+#include "keyboard.h"
+#include "memory.h"
+#include "ports.h"
+#include "tape.h"
+#include "z80.h"
+
+#define code "roms/48.rom"
+
+using namespace std;
+
+class Emulator {
+public:
+	Emulator(int scale, string filename);
+	~Emulator();
+
+	void	handleEvents(SDL_Event &e);
+	void	run();
+	
+private:
+	Logger*      logger;
+	Keyboard*    keyboard;
+	Ula*         ula;
+	Z80*         z80;
+	Mem*         mem;
+	Ports*       ports;
+	Tape*        tape;
+
+	bool         step;
+	bool         interrupts;	
+};
